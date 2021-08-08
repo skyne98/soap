55ebc872f777f5d98b6856601bf1268b4a6d6218
Aleksey Halahan <ahalahan@gmail.com>
2021-08-08 03:09:18 +0200

    Done work on navigation example

--- example/navigation.rs
+++ example/navigation.rs
@@ -1,6 +1,18 @@
 use nannou::prelude::*;
 
-struct Model {}
+const DIAMETER: f32 = 64.0;
+
+struct Model {
+    pub x: f32,
+    pub y: f32,
+    // Navigation
+    pub navigating_to: Option<(f32, f32)>,
+    // Environment
+    pub red_circle: (f32, f32),
+    pub red_circle_interacted: bool,
+    pub green_circle: (f32, f32),
+    pub green_circle_interacted: bool,
+}
 
 fn main() {
     nannou::app(model)
@@ -11,7 +23,15 @@ fn main() {
 }
 
 fn model(app: &App) -> Model {
-    Model {}
+    Model {
+        x: 0.0,
+        y: 0.0,
+        navigating_to: None,
+        red_circle: (256.0, 0.0),
+        red_circle_interacted: false,
+        green_circle: (0.0, 256.0),
+        green_circle_interacted: false,
+    }
 }
 
 fn update(app: &App, model: &mut Model, update: Update) {}
@@ -22,12 +42,28 @@ fn view(app: &App, model: &Model, frame: Frame) {
     // get canvas to draw on
     let draw = app.draw();
 
-    // draw circle
+    // draw character
     draw.ellipse()
         .color(STEELBLUE)
-        .w(64.0)
-        .h(64.0)
-        .x_y(200.0, -100.0);
+        .w(DIAMETER)
+        .h(DIAMETER)
+        .x_y(model.x, model.y);
+
+    // draw goals
+    if model.red_circle_interacted == false {
+        draw.ellipse()
+            .color(ORANGERED)
+            .w(DIAMETER)
+            .h(DIAMETER)
+            .x_y(model.red_circle.0, model.red_circle.1);
+    }
+    if model.green_circle_interacted == false {
+        draw.ellipse()
+            .color(GREENYELLOW)
+            .w(DIAMETER)
+            .h(DIAMETER)
+            .x_y(model.green_circle.0, model.green_circle.1);
+    }
 
     // set background to blue
     draw.background().color(PINK);
